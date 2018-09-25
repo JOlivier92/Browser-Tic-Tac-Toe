b7b50801176faf2bcb274df20ddab800609e46f5
Joseph Olivier <josepholivier92@gmail.com>
2018-09-25 16:07:03 -0700

    finish playability for tic tac toe

---
+++ Tic-Tac-Toe/js/main.js
@@ -0,0 +1,8 @@
+const View = require('./ttt-view');// require appropriate file
+const Game = require('../solution/game');// require appropriate file
+
+$( () => {
+  const rootEl = $('.ttt');
+  const game = new Game();
+  new View(game, rootEl);
+});
---
+++ Tic-Tac-Toe/js/ttt-view.js
@@ -0,0 +1,56 @@
+class View {
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
+
+    this.setupBoard();
+    this.bindEvents();
+  }
+
+  bindEvents() {
+    this.$el.on ("click","li", (event => {
+      let square = $(event.currentTarget);
+      this.makeMove(square);
+    }));
+  }
+
+  makeMove($square) {
+    // alert("clicked")
+    let currentPlayer = this.game.currentPlayer;
+    try {
+      this.game.playMove($square.data("pos"));
+    } catch (exception) {
+      alert(exception.msg);
+      return ;
+    }
+
+    $square.addClass(currentPlayer);
+
+    if (this.game.isOver()) {
+      this.$el.off("click");
+      this.$el.addClass("over");
+
+      let winner = this.game.winner();
+      // const $figcaption = $(<"figcaption">);
+      if (winner) {
+        // $figcaption.html("winner")
+      } else {
+        // $figcaption.html("draw")
+      }
+    }
+  }
+
+  setupBoard() {
+    const $ul = $("<ul>");
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        let $li = $("<li>");
+        $li.data("pos", [i, j]);
+        $ul.append($li);
+      }
+    }
+    this.$el.append($ul);
+  }
+}
+
+module.exports = View;
